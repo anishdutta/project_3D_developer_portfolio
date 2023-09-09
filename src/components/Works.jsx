bb9f7347612878d2a07c0b7665e29d1b22e21224
Anish Dutta <anish2000.ad@gmail.com>
2023-09-10 01:47:14 +0530

    minor mobile fixes done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,13 +17,9 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("left", "spring", index * 0.5, 0.75)}>
       <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+        
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
@@ -81,14 +77,13 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant(0)}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
@@ -108,4 +103,4 @@ const Works = () => {
   );
 };
 
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "works");
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -37,8 +37,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 1.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, 0]}
+        scale={isMobile ? 1.5 : 1.75}
+        position={isMobile ? [0, -2, -2.2] : [0, -3.25, 0]}
         rotation={[0, 100, 0]}
       />
     </mesh>
